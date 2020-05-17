40c75932c9c1910bedbae4a8ac9978f66ec234eb
SirJoraH21 <mr.vip.yurra90@gmail.com>
2020-05-17 08:27:59 +0300

    added auth middleware and schema for link

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const CreatePage = () => {
     return (
-        <div>
-            <h1>Create Page</h1>
+        <div className='row'>
+            <div className='col s8 offset-s2' style={{paddingTop: '2rem'}}>
+            <div className="input-field">
+                <input
+                  placeholder="Вставте посилання"
+                  id="link"
+                  type="text"
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+            </div>
         </div>
     )
 }
---
+++ middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+const  jwt = require('jsonwebtoken')
+const config = require('config')
+
+module.exports = (req, res, next) => {
+if (req === 'OPTIONS'){
+    return next()
+}
+
+try {
+
+    const token = req.headers.authorization.split(' ')[1] // Bearer Token
+
+    if (!token) {
+        return res.status(401).json({ message: 'Відстня авторизація' })
+    }
+
+    const decoded = jwt.verify(token, config.get('jwtSecret'))
+    req.user = decoded
+    next()
+
+} catch (e) {
+    return res.status(401).json({ message: 'Відстня авторизація' })
+}
+
+
+}
