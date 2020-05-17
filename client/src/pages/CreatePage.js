f2302edfafb524e70b99558cddfe7a16d1ae5605
SirJoraH21 <mr.vip.yurra90@gmail.com>
2020-05-17 08:47:27 +0300

    createPage works

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,6 +1,31 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+import {useHttp} from '../hooks/http.hook'
+import { AuthContext } from '../context/AuthContext'
+import { useHistory } from 'react-router-dom'
 
 export const CreatePage = () => {
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+    const {request} = useHttp()
+    const [link, setLink] = useState('')
+
+    useEffect(() => {
+        window.M.updateTextFields()
+    }, [])
+
+    const pressHandler = async event => {
+        if (event.key === 'Enter') {
+            try {
+                const data = await request('api/link/generate', 'POST', {from: link}, {
+                    Authorization: `Bearer ${auth.token}`
+                })
+                history.push(`detail/${data.link._id}`)
+            } catch (e) {
+
+            }
+        }
+    }
+
     return (
         <div className='row'>
             <div className='col s8 offset-s2' style={{paddingTop: '2rem'}}>
@@ -9,9 +34,11 @@ export const CreatePage = () => {
                   placeholder="Вставте посилання"
                   id="link"
                   type="text"
-                  onChange={changeHandler}
+                  value={link}
+                  onChange={e => setLink(e.target.value)}
+                  onKeyPress={pressHandler}
                 />
-                <label htmlFor="email">Email</label>
+                <label htmlFor="link">Введіть посилання</label>
               </div>
             </div>
         </div>
